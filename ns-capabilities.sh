eea35e8f4329cc9da56a84af45c5184faa8a2cbf
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2026-04-27 15:16:42 +0200

    feat: ns cps in ns manifest
    Signed-off-by: Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>

--- ns-capabilities.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Label a namespace to enforce the restricted profile
-kubectl label namespace hello-world \
-  pod-security.kubernetes.io/enforce=restricted \
-  pod-security.kubernetes.io/enforce-version=latest \
-  pod-security.kubernetes.io/warn=restricted \
-  pod-security.kubernetes.io/audit=restricted
